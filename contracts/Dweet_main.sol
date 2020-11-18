280888322f5e6f2f3f16e5753266753c9109feb3
AdityaAshutosh <adityaashutosh82@gmail.com>
2020-11-18 17:42:01 +0530

    Modular objects/ Boilerplate code

---
+++ contracts/Dweet_main.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.5.0;
+
+contract DweetSent {
+
+    struct User {
+        uint age;
+        string EthAddress;
+        uint index;
+        uint DweetCount;
+        mapping (uint => Dweet) MyDweets;
+        Dweet[] DweetsArray;
+    }
+    mapping (address => User) public _user;
+    
+    address[] user _index;
+    address public owner;
+    
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    //OAuth Sign In via MetaMask to be figured out.(event specifier and SSO plugin if needed?)
+    
