d6c7f25c912707694662aa28011a6eada29fa6d7
Rachitabarla <rachita.barla2001@gmail.com>
2020-11-20 08:36:36 +0530

    create dweet code written

--- contracts/Dweet_main.sol
+++ contracts/Dweet_main.sol
@@ -1,29 +1,46 @@
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.1;
 
 contract DweetSent {
-
+    uint public DweetCount;
+    uint256 public timestamp;
+     struct Dweet 
+     {
+        uint256 timestamp;
+	    string dweetString;
+     }
     struct User {
         uint age;
         string EthAddress;
         uint index;
-        uint DweetCount;
-        mapping (uint => Dweet) MyDweets;
         Dweet[] DweetsArray;
     }
+    mapping (uint => Dweet) public MyDweets;
     mapping (address => User) public _user;
     
-    address[] user _index;
+    address [] userindex;
     address public owner;
     
     
     constructor() public {
+        createDweet("Hello World");
         owner = msg.sender;
+        timestamp=1544668513;
     }
+    
+    
     
     modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-    
-    //OAuth Sign In via MetaMask to be figured out.(event specifier and SSO plugin if needed?)
-    
+    event DweetCreated(
+        uint timestamp,
+        string dweetString);
+        
+    function createDweet(string memory _dweetString) public{
+       DweetCount++;
+       timestamp = block.timestamp;
+       MyDweets[DweetCount] = Dweet(timestamp,_dweetString);
+       emit DweetCreated(timestamp,_dweetString);
+       }
+}
