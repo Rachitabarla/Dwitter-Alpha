adfaeda40c9c96421dcc189e20975649c08d72df
Zen-trepreneur <59253439+Zen-trepreneur@users.noreply.github.com>
2020-11-21 22:08:46 +0530

    Update Dweet_main.sol

--- Dweet_main.sol
+++ Dweet_main.sol
@@ -34,16 +34,26 @@ contract DweetSent {
     
     mapping (address => uint[]) public dweets_via_author;
     
+     function addUser (string memory _EthAddress, uint _age) public returns (bool success)  {
+        address Address = msg.sender;
+        require(isUser(Address) == false); 
+        users[Address].age = _age;
+        users[Address].EthAddress = _EthAddress;
+        users[Address].index = user_index.push(Address) - 1;
+        emit LogNewUser(Address, users[Address].index, _EthAddress, _age);
+        return true;
+    }
+     
      function createDweet(string memory _text) public returns (bool) {
         require (isUser(msg.sender) == true);
         uint dweet_index;
         dweet_index = all_dweets.push(Dweet(_text, msg.sender))-1;
 
-        uint _count = users[msg.sender].my_dweet_count;
-        users[msg.sender].my_dweets[_count] = Dweet(_text, msg.sender);
-        users[msg.sender].my_dweet_count ++;
+        uint _count = users[msg.sender].DweetCount;
+        users[msg.sender].MyDweets[_count] = Dweet(_text, msg.sender);
+        users[msg.sender].DweetCount ++;
 
-        users[msg.sender].my_dweets_array.push(Dweet(_text, msg.sender));
+        users[msg.sender].DweetsArray.push(Dweet(_text, msg.sender));
 
         return true;
     }
