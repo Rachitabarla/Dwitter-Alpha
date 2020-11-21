842d442ef2dc2330aa8ec2ea107ba4fa0526f4df
Rachitabarla <rachita.barla2001@gmail.com>
2020-11-22 00:38:52 +0530

    created report and like functions

--- Dweet_main.sol
+++ Dweet_main.sol
@@ -1,99 +1,103 @@
-pragma solidity >=0.5.0;
+pragma solidity >=0.5.1;
 
 contract DweetSent {
-
-    struct Dweet 
+    uint public DweetCount;
+    uint256 id;
+    uint public lCount = 0;
+    uint public rCount = 0 ;
+     struct Dweet 
      {
-        uint timestamp;
-	string dweetString;
-	address dweetAuthor;
-	string dweet_tags;
-	 
-	
+        uint256 id;
+	    string dweetString;
+	    uint lCount;
      }
     struct User {
         uint age;
         string EthAddress;
+        string email;
         uint index;
-        uint DweetCount;
-        mapping (uint => _dweets) MyDweets;
         Dweet[] DweetsArray;
     }
-    mapping (address => _user) public _user;
-    address[] userindex;
-    address public owner;
+    mapping (uint => Dweet) public MyDweets;
+    mapping (address => User) user;
+    mapping(uint => uint) reportCount;
+    mapping(address => bool) reportAddress;
+    mapping (address => uint) like;
     
+    address [] userindex;
+    address payable owner = msg.sender;
+    bool action = false;
     
     constructor() public {
-        owner = msg.sender;
+        createDweet("Hello World");
     }
-    
-    modifier onlyOwner() {
+        modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
-    uint private totalDweetsCount;
-    Dweet[] public RepoDweets;
-    
-    mapping (address => uint[]) public dweets_via_author;
-    
-    //Take Mohit's confirmations for monitoring page as well as tagging. Else, tagging could be low priority.
-    event LogNewUser (address indexed Address, uint index, string EthAddress, uint age);
-    event LogUpdateUser (address indexed Address, uint index, string EthAddress, uint age);
-    event NewDweet (address indexed Address, uint )
-
-    function isUser(address Address) public view returns (bool yesIsUser){
-        if(userindex.length == 0) return false;
-        return (userindex[users[Address].index] == Address); 
-    
-     function addUser (string memory _EthAddress, uint _age) public returns (bool success)  {
-        address Address = msg.sender;
-        require(isUser(Address) == false); 
-        users[Address].age = _age;
-        users[Address].EthAddress = _EthAddress;
-        users[Address].index = userindex.push(Address) - 1;
-        emit LogNewUser(Address, users[Address].index, _EthAddress, _age);
-        return true;
-    }
-    
-    function getUser(address Address) public view returns(string memory EthAddress, uint age, uint index) {
-        require(isUser(Address) == true);
-        return (users[Address].EthAddress, users[Address].age, users[Address].index);
-    }
-
-     
-     function createDweet(string memory _text) public returns (bool) {
-        require (isUser(msg.sender) == true);
-        uint dweet_index;
-        dweet_index = RepoDweets.push(Dweet(_text, msg.sender))-1;
-
-        uint _count = users[msg.sender].DweetCount;
-        users[msg.sender].MyDweets[_count] = Dweet(_text, msg.sender);
-        users[msg.sender].DweetCount ++;
-
-        users[msg.sender].DweetsArray.push(Dweet(_text, msg.sender));
-
-        return true;
-    }
-    
-    function dweetsByUser(address Address) public view returns (Dweet[] memory) {
-        require (isUser(Address) == true);
-
-        Dweet[] memory _dweets = new Dweet[](users[Address].dweet_count);
+    event DweetCreated(
+        uint id,
+        string dweetString);
         
-        return users[Address].DweetsArray;
+    function createDweet(string memory _dweetString) public onlyOwner{
+       DweetCount++;
+       id +=1;
+       MyDweets[DweetCount] = Dweet(id,_dweetString,0);
+       emit DweetCreated(id,_dweetString);
+       }
+       
+       event Reported(
+           uint id,
+           string dweetString);
+           
+     function reportOnce(uint _id, address _ethaddress) external {
+        
+         if (reportAddress[_ethaddress]!= true && reportCount[_id]< 1){
+            reportDweet(_id,_ethaddress);
+         }
+     }
+     
+     function reportDweet(uint _id,address _ethaddress) internal {
+         rCount+=1;
+         reportCount[id] = rCount;
+         
+         if ((reportCount[id])>=10) {
+            MyDweets[_id].dweetString = "";
+            rCount = 0;
+            reportAddress[_ethaddress] = true;
+         }
+         emit Reported(id, MyDweets[_id].dweetString);
+     }
+     
+    event profileUpdate (
+        uint age,
+        string email); 
+    
+    function editprofile(address _address, uint _age, string memory _email) public onlyOwner{
+        
+        require(_address == msg.sender);
+        user[_address].age = _age;
+        user[_address].email = _email;
+        emit profileUpdate(user[_address].age ,user[_address].email);
     }
     
-    function totalDweetsByUser (address Address) public view returns (uint) {
-        return users[Address].dweet_count;
-    }
-
-    function dweetByIndex(uint _id) public view returns (string memory _dweet, address _author) {
-        _dweet = RepoDweets[_id].dweet_text;
-        _author = RepoDweets[_id].dweet_author;
-        return  (_dweet, _author);
-    }
+    event liked(
+        uint id,
+        uint likeCount
+        );
+     function likeOnce(uint _id, address _ethaddress) external {
+        
+         if (like[_ethaddress] < 1 ){
+            likeDweet(_id,_ethaddress);
+         }
+     }
+         
+         function likeDweet(uint _id , address _ethaddress) internal {
+             like[_ethaddress] += 1;
+             lCount+=1;
+             //like[lCount] += 1;
+             MyDweets[_id].lCount += 1;
+             emit liked(_id,MyDweets[_id].lCount);
+         }
+     }
     
-    
-    
-}
